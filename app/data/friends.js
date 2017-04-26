d8d3342db54d86f84bbb9cdd9bcb91c4a11d6e9f
CodingKaiser <falkonoe@live.com>
2017-04-25 21:28:42 -0700

    Got most of it working.

--- app/data/friends.js
+++ app/data/friends.js
@@ -10,6 +10,22 @@ class FriendsDb {
   getFriends() {
     return this.friends;
   }
+
+  getClosestMatch(stranger) {
+    var bestFriend = this.getFriends()[0];
+    var bestScore = Number.MAX_VALUE;
+    for (potentialFriend of this.getFriends()) {
+      var sumOfDiff = 0;
+      for (var i = 0; i < stranger.scores; i++) {
+        sumOfDiff += Math.abs(stranger.scores[i] - potentialFriend.scores[i]);
+      }
+      if (sumOfDiff < bestScore) {
+        bestFriend = potentialFriend;
+        bestScore = sumOfDiff;
+      }
+    }
+    return bestFriend;
+  }
 }
 
 module.exports = FriendsDb;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,12 +1,19 @@
 function loadApiRoutes(app, friends) {
+  function isValidFriend(friend, numOfQuestions) {
+    return (friend.name && friend.photo && friend.scores && friend.scores.length === numOfQuestions);
+  }
+
   app.get("/api/friends", (req, res) => {
     return res.json(friends.getFriends());
   });
 
   app.post("/api/friends", (req,res) => {
     var newFriend = req.body;
-    friends.add(newFriend);
-    return res.json(newFriend);
+    if (isValidFriend(newFriend, 10)) {
+      friends.add(newFriend);
+      return res.json(newFriend);
+    }
+    return res.json({error: "Object did not have correct values or number of keys"})
   });
 }
 
