b3a7f386f80dc02fde22a2d45ebe981d0a62cbd8
CodingKaiser <falkonoe@live.com>
2017-04-29 12:58:19 -0700

    Populate table on server initialization

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,6 +1,22 @@
 class FriendsDb {
   constructor() {
-    this.friends = [];
+    this.friends = [
+      {
+        name: "Donald Trump",
+        photo: "https://static01.nyt.com/images/5016/02/03/opinion/3rosenthalWEB/3rosenthalWEB-blog480.jpg",
+        scores: ["1","1","1","1","1","1","1","1","1","1"]
+      },
+      {
+        name: "Anthony Weiner",
+        photo: "https://nyoobserver.files.wordpress.com/2013/04/anthony-weiner-twitter.jpeg",
+        scores: ["2","2","2","2","2","2","2","2","2","2"]
+      },
+      {
+        name: "Hillary Clinton",
+        photo: "https://static5.businessinsider.com/image/571fd06552bcd0210c8bf0e4-480/hillary-clinton.jpg",
+        scores: ["5","5","5","5","5","5","5","5","5","5"]
+      }
+    ];
   }
 
   add(friend) {
